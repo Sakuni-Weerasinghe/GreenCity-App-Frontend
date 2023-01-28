c6197a4a1e5aa36428e087dd5256ce3790a4cc95
Sakuni <S17163@sci.pdn.ac.lk>
2023-01-28 20:07:19 +0530

    Create CollectionCenterDeleteModal.tsx
    Collection Center Delete Modal Created

---
+++ src/Modal/CollectionCenterDeleteModal.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Modal from "react-bootstrap/Modal";
+import { useNavigate } from 'react-router-dom';
+
+
+const style = {
+    button: {
+        border: "none",
+        width: "20%",
+    },
+};
+
+const CollectionCenterDeleteModal = (props: any) => {
+    const navigate = useNavigate();
+
+    const modalStateHandler = (value: string) => {
+        if (value === "Yes") {
+            props.onHide();
+            navigate("/");
+        } else if (value === "No") {
+            props.onHide();
+            navigate("/collectionCenter");
+        }
+    };
+
+    return (
+        <>
+            <Modal {...props} size="md" aria-labelledby="contained-modal-title-vcenter" centered>
+                <Modal.Header closeButton></Modal.Header>
+                <Modal.Body>
+                    <h6>Are you sure do you want to delete collection center?</h6>
+                </Modal.Body>
+                <Modal.Footer>
+                    <button onClick={() => modalStateHandler("No")} className="btn btn-dark  px-2 my-3 " style={style.button}> No </button>
+                    <button onClick={() => modalStateHandler("Yes")} className="btn btn-danger  px-2 my-3 " style={style.button}> Yes </button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+};
+
+export default CollectionCenterDeleteModal
