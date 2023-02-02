001ab85fd652e4202aa8aca66f36fae6a5ae8d34
Sakuni <S17163@sci.pdn.ac.lk>
2023-02-02 23:24:21 +0530

    Create Collection Center Profile Page and Collection center detail update page

--- src/Pages/CollectionCenter/center.tsx
+++ src/Pages/CollectionCenter/center.tsx
@@ -3,51 +3,40 @@ import { useState, useEffect } from "react";
 import { getCenterBoard } from "../../services/user.service";
 import center from "../../assets/Images/center1.jpg";
 import "./collectioncenter.css";
-import { useNavigate } from "react-router-dom";
-import CollectionCenterDeleteModal from "../../Modal/CollectionCenterDeleteModal";
+import { useNavigate, useLocation } from "react-router-dom";
+import { collectionCenterProfileDetails, getCurrentCollectionCenterProfileDetails } from "../../services/profileManagement.service";
 
-const BoardCenter = () => {
+const BoardCenter = (props: any) => {
   let navigate = useNavigate();
-  const [showCollectionCenterDeleteModal, setShowCollectionCenterDeleteModal] = useState(false)
+  const { loginStatus } = props;
+  const location = useLocation();
+  const username = location.state.parameter;
 
-  // const [content, setContent] = useState<string>("");
-
+  collectionCenterProfileDetails(username, "COLLECTION_CENTER");
   // useEffect(() => {
-  //   getCenterBoard().then(
-  //     (response) => {
-  //       setContent(response.data);
-  //     },
-  //     (error) => {
-  //       const _content =
-  //         (error.response &&
-  //           error.response.data &&
-  //           error.response.data.message) ||
-  //         error.message ||
-  //         error.toString();
 
-  //       setContent(_content);
-  //     }
-  //   );
-  // }, []);
+  const collectionCenterDetails = getCurrentCollectionCenterProfileDetails();
+
+
+  // },[])
 
   const onClickRequest = () => {
-    navigate("/collectionRequest/collectionRequest_requirement");
-  }
-
-  const onClickUpdate = () => {
-    navigate("/collectionCenter/collectionCenter_update");
+    if (loginStatus) {
+      navigate("/collectionRequest/collectionRequest_requirement",
+        { state: { parameter: collectionCenterDetails.username } });
+    } else
+      alert("Please Login Your Profile")
   }
 
   return (
     <>
-      <div id="main_banner">
-        {/*<img src={banner} alt="" />*/
-        }
-      </div>
-
+      {/* <div id="main_banner">
+         <img src={banner} alt="" />
+        
+      </div> */}
       <div className="container mt-4">
         <div>
-          <h2>Center Name</h2>
+          <h2>Center Name : {collectionCenterDetails.centerName}</h2>
           <p>Created On: | Created By:</p>
         </div>
 
@@ -58,12 +47,17 @@ const BoardCenter = () => {
           <div className="col-xl-6 col-md-3 col-lg-6 col-sm-12 p-0 my-auto">
             <ul className="list-group text-center">
               <li className="list-group-item text-light p-1 title">Category</li>
-              <li className="list-group-item p-2 data">Glass | Plastic</li>
+              <li className="list-group-item p-2 data">{collectionCenterDetails.wastetype} -
+                Rs.{collectionCenterDetails.payment} for 1kg</li>
+              <li className="list-group-item text-light p-1 title">Address</li>
+              <li className="list-group-item p-2 data">{collectionCenterDetails.addressLine1} ,
+                {collectionCenterDetails.addressLine2} ,
+                {collectionCenterDetails.addressLine3}</li>
               <li className="list-group-item text-light p-1 title">Location</li>
-              <li className="list-group-item p-2 data">Peradeniya, Kandy</li>
-              <li className="list-group-item text-light p-1 title">Contact</li>
-              <li className="list-group-item p-2 data">+94077123456</li>
-              <li className="list-group-item p-2 data">center1@gmail.com</li>
+              <li className="list-group-item p-2 data">{collectionCenterDetails.location}</li>
+              <li className="list-group-item text-light p-1 title">Contact Details</li>
+              <li className="list-group-item p-2 data">{collectionCenterDetails.contactNumber}</li>
+              <li className="list-group-item p-2 data">{collectionCenterDetails.email}</li>
               <li className="list-group-item text-light p-1 title">Working days</li>
               <li className="list-group-item p-2 data">Weekdays</li>
             </ul>
@@ -74,17 +68,10 @@ const BoardCenter = () => {
           <h4 className="col py-3 text-center">Description</h4>
         </div>
         <div>
-          <p className="text-justify p-3 py-1">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur qui ab eveniet quos voluptate ex tenetur repellat aliquid, recusandae, itaque nihil ea! Atque delectus illum asperiores voluptates unde, consequuntur voluptatum?</p>
+          <p className="text-justify p-3 py-1">{collectionCenterDetails.description}</p>
         </div>
         <div>
           <button className="btn btn-dark btn-block px-3 mb-3 mt-1" onClick={onClickRequest}> Request PickUp  </button>
-        </div>
-        <div>
-          <button className="btn btn-dark btn-block px-3 mb-3 mt-1" onClick={onClickUpdate}> Update </button>
-        </div>
-        <div>
-          <button className="btn btn-danger btn-block px-3 mb-3 mt-1" onClick={() => { setShowCollectionCenterDeleteModal(true) }}> Delete </button>
-          <CollectionCenterDeleteModal show={showCollectionCenterDeleteModal} onHide={() => setShowCollectionCenterDeleteModal(false)} />
         </div>
       </div>
     </>
