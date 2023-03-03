7092356804912c5ad3d84a24076bce5d9b121674
Sakuni <S17163@sci.pdn.ac.lk>
2023-03-03 17:30:27 +0530

    added styles to sign-up page

--- src/Pages/SignUp/SignUp.tsx
+++ src/Pages/SignUp/SignUp.tsx
@@ -1,7 +1,9 @@
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import "./signup.css";
+import signUpIcon from '../../assets/Images/sign-up.svg';
 
 export const SignUp = () => {
-
+    const signUpPageConfig = { title: 'Signup', message: 'Please select your role!' };
     const navigate = useNavigate();
 
     /**
@@ -14,9 +16,33 @@ export const SignUp = () => {
     }
 
     return (
-        <div>
-            <button onClick={() => onClickHandler("user")} className="btn btn-dark btn-block px-5 mb-3" > USER </button>
-            <button onClick={() => onClickHandler("collectionCenter")} className="btn btn-dark btn-block px-5" > COLLECTION CENTER </button>
+        <div className='container my-5'>
+            <div className="row justify-content-center">
+                <div className='col-7'>
+                    <div className="card shadow">
+                        <div className="text-center py-5">
+                            <div>
+                                <h3 className="fw-bold mb-3">{signUpPageConfig.title}</h3>
+                                <p className="text-dark">{signUpPageConfig.message}</p>
+                                <img className="sign-up-icon mx-auto d-block" src={signUpIcon} alt='sign-up icon' />
+                            </div>
+                            <div className="row mt-3">
+                                <div className="col-6">
+                                    <button className="btn btn-dark btn-block p-2 w-100 btn-custom-1" onClick={() => onClickHandler("user")}  > USER </button>
+                                </div>
+                                <div className="col-6">
+                                    <button className="btn btn-dark btn-block p-2 w-100 btn-custom-1" onClick={() => onClickHandler("collectionCenter")}  > COLLECTION CENTER </button>
+                                </div>
+                            </div>
+                            <div>
+                                <p className="mb-0 mt-4">Already have an Account?
+                                    <Link className="text-dark fw-bold login-link" to='/login'> Login</Link>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
