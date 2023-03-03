f3d07dbe689b57d48f5de197fc5d5cc52eb05b34
Sakuni <S17163@sci.pdn.ac.lk>
2023-03-03 22:30:19 +0530

    updated thumbnail and styles of home collection center list

--- src/Pages/Home/CollectionCenterList/CollectionCenterList.tsx
+++ src/Pages/Home/CollectionCenterList/CollectionCenterList.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import { CollectionCenterData } from '../../../types/type';
+import thumbnail from '../../../assets/Images/Collection_center_thumbnail.png'
 
 const CollectionCenterList = (props: any) => {
     const { loginStatus, collectionList } = props;
@@ -16,15 +17,14 @@ const CollectionCenterList = (props: any) => {
     }
 
     return (
-        <div className="row row-cols-1 row-cols-md-4 g-4 p-5 pt-2">
+        <div className="row">
             {collectionList.map((center_data: CollectionCenterData) => {
-                const image = `"${center_data.image}"`;
                 return (
-                    <div className="col" >
+                    <div className="col-3 my-3">
                         <div className="card shadow ">
                             <div className="card">
                                 <div onClick={() => navigate("/collectionCenter", { state: { parameter: center_data.username } })}>
-                                    <img src={image} className="card-img-top" alt="..." />
+                                    <img src={thumbnail} className="card-img-top" alt="..." />
                                     <div className="card-body card-sm flex-column">
                                         <h5 className="card-title">{center_data.centerName}</h5>
                                         <p className='card-text'>Category : {center_data.wastetype} <br /> Location : {center_data.location} </p>
