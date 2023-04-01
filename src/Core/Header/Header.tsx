7dda73dd6fc6a3b865fb5d14958c4edf538e2bd3
Sakuni <S17163@sci.pdn.ac.lk>
2023-04-01 12:08:09 +0530

    update header style

--- src/Core/Header/Header.tsx
+++ src/Core/Header/Header.tsx
@@ -15,9 +15,9 @@ const Header = (props: any) => {
 
     return (
         <>
-            <nav id="navbar" className="navbar navbar-expand-lg navbar-dark sticky-top shadow">
+            <nav className="navbar navbar-expand-lg navbar-dark sticky-top shadow">
                 <div className="container-fluid">
-                    <Link className="navbar-brand px-2" to='/'><img src={logo} alt="" height="60" /></Link>
+                    <Link className="navbar-brand" to='/'><img src={logo} alt="" height="60" /></Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
                         aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
@@ -44,10 +44,6 @@ const Header = (props: any) => {
                                 ) : <></>
 
                             }
-                            <li className="nav-item mt-2">
-                                {loginStatus ? (<Link className="nav-link" to={{ pathname: "/profile/" + username }}>{username}</Link>)
-                                    : <></>}
-                            </li>
                             <li className="nav-item mt-2">
                                 {loginStatus ? (<Link className="nav-link" to='/login' onClick={() => logOut()}>Logout</Link>)
                                     : (<Link className="nav-link" to='/login'>Login</Link>)}
